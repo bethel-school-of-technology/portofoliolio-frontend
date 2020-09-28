c911ec5e05bc989ab79788bceb91c761155b3ee4
Josie Martinez <jo1432may@gmail.com>
2020-09-28 11:28:29 -0700

    integration user

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,8 +7,11 @@ function AboutPage() {
       <h1>About Us</h1>
       <br />
       <p>
-        We are Number ! Company in the world of making resume . Our team in the
-        most proffetionl
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ return (
 <footer className = 'mt-5'>
 <Container fluid ={true}>
 <Row className = 'border-top justify-content-between p-3'>
-<Col className= 'p-0' md={3} sm={12}>Josie Martinez</Col>
+<Col className= 'p-0' md={3} sm={12}>Josie</Col>
 <Col className = ''> Nick</Col>
 <Col className=''>Jeff</Col>
 <Col className = ''> Ruvim</Col>
